986921c0fc7382263981099fe390f305827b103d
Patrick M <patrickmacdonald07@gmail.com>
2022-09-21 16:40:11 -0400

    made code modular

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
